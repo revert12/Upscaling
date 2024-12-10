3e25d4cfe4272e0e1b7eb844d42d4c9743dbc33f
revert12 <97959984+revert12@users.noreply.github.com>
2024-12-10 16:55:00 +0900

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -10,8 +10,25 @@ pip3 install torch==2.0.1 torchvision==0.15.2 --extra-index-url https://download
 
 # 3. Real-ESRGAN 리포지토리 클론
 echo "Cloning Real-ESRGAN repository..."
-git clone https://github.com/xinntao/Real-ESRGAN.git
-cd Real-ESRGAN
+# git clone https://github.com/xinntao/Real-ESRGAN.git
+
+# 다운로드할 레포지토리 URL
+repo_url="https://github.com/xinntao/Real-ESRGAN/archive/refs/heads/master.zip"
+
+# 저장할 파일 이름
+zip_file="Real-ESRGAN-master.zip"
+
+# ZIP 파일 다운로드
+echo "Downloading $repo_url..."
+wget -O $zip_file $repo_url
+
+# 압축 해제
+echo "Extracting $zip_file..."
+unzip -o $zip_file
+
+# 압축 해제 후 다운로드된 ZIP 파일 삭제 (원하는 경우)
+rm $zip_file
+cd Real-ESRGAN-master
 
 # 4. 필요한 패키지 설치
 echo "Installing required libraries..."
